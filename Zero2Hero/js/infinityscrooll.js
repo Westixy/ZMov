033f5f5e6d93e36a4fdcfb1782e39550674d3a92
ddillenbourg <dinesh.dillenbourg@gmail.com>
2016-09-30 10:24:08 +0200

    Front end Base
    Fichiers Dinesh - front end

---
+++ Zero2Hero/js/infinityscrooll.js
@@ -0,0 +1,19 @@
+var max = 20;
+
+var loadMore = function () {
+  for (var i = max-20; i < max; i++) {
+    $(".inner").append("<p>test "+i+"</p>");
+  }
+  max += 20;
+}
+
+$('.on').scroll(function () {
+  var top = $('.on').scrollTop();
+  $('.two').html("top: "+top+" diff: "+($(".inner").height() - $(".on").height()));
+  if (top >= $(".inner").height() - $(".on").height()) {
+    $('.two').append(" bottom");
+    loadMore();
+  }
+});
+
+loadMore();
