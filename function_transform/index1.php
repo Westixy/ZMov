d88b7a2b56cfe4bcc7bb73f33338df0ea6787c12
Negaohm <joel.DE-SOUSA@cpnv.ch>
2016-11-04 10:57:30 +0100

    Edit nameTransform function
    nameTransform function return a json list and the function read the $_POST value

--- function_transform/index1.php
+++ function_transform/index1.php
@@ -1,34 +1,36 @@
 <?php
-	require('info.php'); // relie la page de la foncion API
-	
+
+// TODO : preg_match api !
+
+
+require('info.php'); // relie la page de la foncion API
+
+	// transforme le nom des films dans $_POST et retourne une chaine json
 	function nameTransform(){
 		
-		// récupère le fichier json avec la liste des films
-		$tmp = file_get_contents("list.json");
-		$tmp = json_decode($tmp, true);
+		$_POST[0] = 'test.avi';
+		$_POST[1] = 'test2-lol.mkv';
 		
 		// active le mode delete --> écrire dans l'url ?mode=delete pour supprimer des mot clés
 		if(isset($_GET['mot']) && isset($_GET['mode']) && $_GET['mode'] == 'delete'){
 			getforgivenWords($_GET['mot']);
 		}
 		
+		$json = array();
 		
-		// parcours chaque film
-		for($i = 0; $i < count($tmp); $i++){
-			$tmp[$i]['name'] = str_replace(".", " ", $tmp[$i]['name']); // remplace les points par des espaces
-			$tmp[$i]['name'] = str_replace("-", " ", $tmp[$i]['name']); // remplace les tirets par des espaces
-			$tmp[$i]['name'] = str_replace("_", " ", $tmp[$i]['name']); // remplace les tirets par des espaces
-			$tmp[$i]['name'] = str_replace("[", "", $tmp[$i]['name']);
-			$tmp[$i]['name'] = str_replace("]", "", $tmp[$i]['name']);
-
-			$film[$i]='';
-			$display = array();
+		for($i = 0; $i < count($_POST); $i++){
+			$_POST[$i] = str_replace(".", " ", $_POST[$i]); // remplace les points par des espaces
+			$_POST[$i] = str_replace("-", " ", $_POST[$i]); // remplace les tirets par des espaces
+			$_POST[$i] = str_replace("_", " ", $_POST[$i]); // remplace les tirets par des espaces
+			$_POST[$i] = str_replace("[", "", $_POST[$i]);
+			$_POST[$i] = str_replace("]", "", $_POST[$i]);
 			
-			$tmp1[$i] = explode(" ", $tmp[$i]['name']); // split les mots séparés par des espaces dans un tableau
+			$film[$i]='';
+			
+			$tmp1[$i] = explode(" ", $_POST[$i]); // split les mots spéarés par des espaces dans un tableau
 			
 			for($d = 0; $d < count($tmp1[$i]); $d++){
-				
-				// Si le mot ne fais pas partis des mot interdits
+				// si le mot ne fais pas partis des mots interdits
 				if(!findWord($tmp1[$i][$d]) && substr($tmp1[$i][$d], 0, -2) != '20'){
 					if(isset($_GET['mode']) && $_GET['mode'] == 'delete'){
 						$film[$i] .= '<a href="index.php?mode=delete&mot='.$tmp1[$i][$d].'">'.$tmp1[$i][$d]." </a>";
@@ -37,20 +39,15 @@
 					}
 				}
 			}
-			echo $film[$i] . '<br />';
-			if(isset($_POST['entree']) && $_POST['entree'] != ''){
-				if(preg_match(strtoupper("/".$_POST['entree']."/"), strtoupper($film[$i]))){
-					api_search($film[$i]);
-				}
-			}
+			$json[$i] = $film[$i];
 		}
+		
+		return json_encode($json);
 	}
-	
-	// vérifie si le mot en paramètre fait partie de la liste
+// vérifie si le mot en paramètre fait partie de la liste
 	function findWord($word){
 		$list = file_get_contents('forgivenList.json'); // récupère liste de mot interdits
 		$list = json_decode($list, true);
-
 		// tableau de mots à enlever
 		$forgivenWords = $list;
 		
@@ -64,6 +61,7 @@
 		return false;
 	}
 	
+	
 	function getforgivenWords($mot){
 		$list = fopen('forgivenList.json','r+');
 		$content = fgets($list);
@@ -71,9 +69,3 @@
 		fputs($list, ',"'.$mot.'"]');
 		fclose($list);
 	}
-?>
-<form action="#" method="post">
-	<input type="text" name="entree"></input>
-	<input type="submit"></input>
-</form>
-<div><?php nameTransform();?></div>
---
+++ function_transform/search.php
@@ -0,0 +1,4 @@
+<?php
+	require('index1.php');
+	
+	echo nameTransform();
