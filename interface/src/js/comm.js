c80a6c48116faa57593548a91e8c730f6e35b2aa
Esteban Sotillo <esteban.sotillo@cpnv.ch>
2016-11-18 12:37:22 +0100

    CommEmitter v0.1.0 finished

--- interface/src/js/comm.js
+++ interface/src/js/comm.js
@@ -1,8 +1,61 @@
-function Comm(id,recive,send){
+/* Created by Esteban Sotillo <westixy@gmail.com>
+      Name : CommEmmiter
+      Version : 0.1.0
+      date : 18.11.2016
+*/
+
+/*
+ * CommEmmiter est une classe immitant le fonctionnnement d'un emmitter
+ * en node JS. cependant, il travaille sur deux divs affin de pouvoir
+ * communiquer entre l'extention firefox et le navigateur.
+ *
+ * Il est nécessaire de faire un init() pour que l'evenement d'écoute sur
+ * la div s'enclenche.
+ *
+ * chaque éléments de communication à un id , une div de réception (celle ou
+ * il va écouter les changements qui opèrent), un div d'emission (qui va
+ * recevoir les données a transmettre et qui seras aussi ecoutée par le
+ * deuxieme Comm (ou plus si il y en a plus :D)
+ *
+ * Etant donné que chaque Comm à un id particulier, il est obligatoire de dire
+ * à qui l'on veut envoyer un emit. (emit(who,what[,arg])) (arg est une seule
+ * variable)
+ *
+ * NB : éviter les fonctions anonymes dans les .on, il n'est pas possible de
+ *      faire des .off après si elles n'ont pas de nom (à améliorer :) )
+ *
+ * ENJOY IT ;)
+ * Westixy
+ *
+**/
+
+/* LICENCE WTFPL
+
+  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+          Version 2, December 2004
+
+  Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+  Everyone is permitted to copy and distribute verbatim or modified
+  copies of this license document, and changing it is allowed as long
+  as the name is changed.
+
+  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+*/
+
+function CommEmmiter(id,recive,send){
   var that=this;
 
-  this.r=document.querySelector(recive);
-  this.s=document.querySelector(send);
+  this.css={
+    r:recive,
+    s:send
+  }
+  this.r=document.querySelector(that.css.r);
+  this.s=document.querySelector(that.css.s);
 
   this.id=id;
 
@@ -17,31 +70,36 @@ function Comm(id,recive,send){
 
 
   this.emit=function(who,action,vars){
-    this.sample.id=who;
-    this.sample.action=action;
-    this.sample.vars=vars;
-    this.s.innerHTML=JSON.stringify(this.sample);
+    that.sample.id=who;
+    that.sample.action=action;
+    that.sample.vars=(typeof vars!="undefined")?vars:null;
+    that.s.innerHTML=JSON.stringify(that.sample);
   }
   this.on=function(action,callback){
     if(typeof that.actions[action]=="undefined")that.actions[action]=[];
     if(that.actions[action].indexOf(callback)<0)
       that.actions[action].push(callback);
-    console.log(that.actions);
   }
   this.off=function(action,callback){
     if(typeof that.actions[action]=="undefined") return;
     var index = that.actions[action].indexOf(callback);
     if(index>-1)
       that.actions[action].splice(index,1);
-    console.log(that.actions);
   }
 
   this.onRep=function(ev){
-    var d = ev.target.data;
+    var d = JSON.parse(ev.target.innerHTML);
+    if(d.id!=that.id) return;
+    if(typeof that.actions[d.action]=="undefined") return;
+    for(var i=0 ; i<that.actions[d.action].length ; i++){
+      that.actions[d.action][i](d.vars);
+    }
   }
 
   this.init=function(){
-    this.r.addEventListener('DOMSubtreeModified',that.onRep,false);
+    that.r=document.querySelector(that.css.r);
+    that.s=document.querySelector(that.css.s);
+    that.r.addEventListener('DOMSubtreeModified',that.onRep);
   }
 }
-Comm.version='0.1.0';
+CommEmmiter.version='0.1.0';
